1037f47249061bfe64710de19f34d798286e418f
Your Name <you@example.com>
2024-12-31 14:44:42 -0500

    codebase

---
+++ src/main/java/com/johanGroup/RabbitMQ_spring/RabbitMqSpringApplication.java
@@ -0,0 +1,13 @@
+package com.johanGroup.RabbitMQ_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johanGroup/RabbitMQ_spring/RabbitMqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanGroup.RabbitMQ_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
